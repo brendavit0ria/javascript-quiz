219aaa38440f30256a7cded5b3f902e697904a45
Brenda Vitória <brendavitoria0602@gmail.com>
2024-02-06 13:33:48 -0300

    initial commit

---
+++ js/script.js
@@ -0,0 +1,111 @@
+const questions = [
+  {
+    question: "Qual método é usado para adicionar um evento a um elemento HTML em JavaScript?",
+    answers: [
+      "addEvent()",
+      "addEventListener()",
+      "attachEvent()"
+    ],
+    correct: 1 
+  },
+  {
+    question: "Qual é o operador lógico 'E' em JavaScript?",
+    answers: [
+      "&&",
+      "||",
+      "!"
+    ],
+    correct: 0 
+  },
+  {
+    question: "Qual função é usada para imprimir algo no console em JavaScript?",
+    answers: [
+      "print()",
+      "log()",
+      "console.log()"
+    ],
+    correct: 2 
+  },
+  {
+    question: "Qual dos seguintes não é um tipo de dados primitivo em JavaScript?",
+    answers: [
+      "boolean",
+      "number",
+      "array"
+    ],
+    correct: 2 
+  },
+  {
+    question: "Qual símbolo é usado para comentários de uma linha em JavaScript?",
+    answers: [
+      "//",
+      "/*",
+      "#"
+    ],
+    correct: 0 
+  },
+  {
+    question: "Qual é a função usada para converter uma string em um número em JavaScript?",
+    answers: [
+      "parseString()",
+      "toInt()",
+      "parseInt()"
+    ],
+    correct: 2 
+  },
+  {
+    question: "Qual método é usado para remover o último elemento de um array em JavaScript?",
+    answers: [
+      "removeLast()",
+      "pop()",
+      "deleteLast()"
+    ],
+    correct: 1 
+  },
+  {
+    question: "Qual é a forma correta de se referir a um elemento HTML pelo ID em JavaScript?",
+    answers: [
+      "getElementByID()",
+      "selectByID()",
+      "document.getElementById()"
+    ],
+    correct: 2 
+  },
+  {
+    question: "Qual método é usado para adicionar um elemento ao final de um array em JavaScript?",
+    answers: [
+      "push()",
+      "append()",
+      "add()"
+    ],
+    correct: 0 
+  },
+  {
+    question: "Qual operador é usado para verificar se dois valores são iguais em valor e tipo em JavaScript?",
+    answers: [
+      "==",
+      "===",
+      "="
+    ],
+    correct: 1 
+  }
+];
+
+const quiz = document.querySelector('#quiz')
+const template = document.querySelector('template');
+
+for (let item of questions) {
+  const quizItem = template.content.cloneNode(true)
+  quizItem.querySelector('h3').textContent = item.question
+
+  for (let answer of item.answers) {
+    const dt = quizItem.querySelector('dl dt').cloneNode(true)
+    dt.querySelector('span').textContent = answer
+
+    quizItem.querySelector('dl').appendChild(dt)
+  }
+
+  quizItem.querySelector('dl dt').remove()
+
+  quiz.appendChild(quizItem)
+}
