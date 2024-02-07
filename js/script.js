4d7b84c7924d85e90fbe4e470edc5927410207fa
Brenda Vitória <brendavitoria0602@gmail.com>
2024-02-07 16:14:44 -0300

    implemented quiz functionality to track correct answers

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ const questions = [
       "addEventListener()",
       "attachEvent()"
     ],
-    correct: 1 
+    correct: 1
   },
   {
     question: "Qual é o operador lógico 'E' em JavaScript?",
@@ -15,7 +15,7 @@ const questions = [
       "||",
       "!"
     ],
-    correct: 0 
+    correct: 0
   },
   {
     question: "Qual função é usada para imprimir algo no console em JavaScript?",
@@ -24,7 +24,7 @@ const questions = [
       "log()",
       "console.log()"
     ],
-    correct: 2 
+    correct: 2
   },
   {
     question: "Qual dos seguintes não é um tipo de dados primitivo em JavaScript?",
@@ -33,7 +33,7 @@ const questions = [
       "number",
       "array"
     ],
-    correct: 2 
+    correct: 2
   },
   {
     question: "Qual símbolo é usado para comentários de uma linha em JavaScript?",
@@ -42,7 +42,7 @@ const questions = [
       "/*",
       "#"
     ],
-    correct: 0 
+    correct: 0
   },
   {
     question: "Qual é a função usada para converter uma string em um número em JavaScript?",
@@ -51,7 +51,7 @@ const questions = [
       "toInt()",
       "parseInt()"
     ],
-    correct: 2 
+    correct: 2
   },
   {
     question: "Qual método é usado para remover o último elemento de um array em JavaScript?",
@@ -60,7 +60,7 @@ const questions = [
       "pop()",
       "deleteLast()"
     ],
-    correct: 1 
+    correct: 1
   },
   {
     question: "Qual é a forma correta de se referir a um elemento HTML pelo ID em JavaScript?",
@@ -69,7 +69,7 @@ const questions = [
       "selectByID()",
       "document.getElementById()"
     ],
-    correct: 2 
+    correct: 2
   },
   {
     question: "Qual método é usado para adicionar um elemento ao final de um array em JavaScript?",
@@ -78,7 +78,7 @@ const questions = [
       "append()",
       "add()"
     ],
-    correct: 0 
+    correct: 0
   },
   {
     question: "Qual operador é usado para verificar se dois valores são iguais em valor e tipo em JavaScript?",
@@ -87,12 +87,17 @@ const questions = [
       "===",
       "="
     ],
-    correct: 1 
+    correct: 1
   }
 ];
 
 const quiz = document.querySelector('#quiz')
-const template = document.querySelector('template');
+const template = document.querySelector('template')
+
+const correct = new Set()
+const totalQuestions = questions.length
+const showTotal = document.querySelector('#hits span')
+showTotal.textContent = correct.size + ' de ' + totalQuestions
 
 for (let item of questions) {
   const quizItem = template.content.cloneNode(true)
@@ -101,6 +106,18 @@ for (let item of questions) {
   for (let answer of item.answers) {
     const dt = quizItem.querySelector('dl dt').cloneNode(true)
     dt.querySelector('span').textContent = answer
+    dt.querySelector('input').setAttribute('name', 'question-' + questions.indexOf(item))
+    dt.querySelector('input').value = item.answers.indexOf(answer)
+
+    dt.querySelector('input').onchange = (event) => {
+      const isValid = event.target.value == item.correct
+
+      correct.delete(item)
+      if (isValid) {
+        correct.add(item)
+      }
+      showTotal.textContent = correct.size + ' de ' + totalQuestions
+    }
 
     quizItem.querySelector('dl').appendChild(dt)
   }
